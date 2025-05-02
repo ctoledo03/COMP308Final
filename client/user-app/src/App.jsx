2d58fccda951f52b5ccf51ed9003e72c9f809575
ctoledo03 <144138050+ctoledo03@users.noreply.github.com>
2025-05-02 18:29:17 -0400

    force redeploy

--- client/user-app/src/App.jsx
+++ client/user-app/src/App.jsx
@@ -8,6 +8,7 @@ const client = new ApolloClient({
   uri: 'http://localhost:4001/graphql', // Set this to your actual GraphQL endpoint
   cache: new InMemoryCache(),
   credentials: 'include'
+  // force redeploy
 });
 
 function App() {
