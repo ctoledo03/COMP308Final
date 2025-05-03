3ee323928a7c190f9db4a8c7842ca1088cf4ab91
ctoledo03 <144138050+ctoledo03@users.noreply.github.com>
2025-05-03 14:50:42 -0400

    bruh /assets/remoteEntry.js was missing

--- client/shell-app/vite.config.js
+++ client/shell-app/vite.config.js
@@ -12,10 +12,10 @@ export default defineConfig({
         userAuth: 'https://communityengagement-clientuserauth.onrender.com/assets/remoteEntry.js',
         // userAuth: 'http://localhost:3001/assets/remoteEntry.js',
 
-        commEngagement: 'https://communityengagement-clientcommengagement.onrender.com',
+        commEngagement: 'https://communityengagement-clientcommengagement.onrender.com/assets/remoteEntry.js',
         // commEngagement: 'http://localhost:3002/assets/remoteEntry.js',
         
-        businessAndEvents: 'https://communityengagement-3cq8.onrender.com'
+        businessAndEvents: 'https://communityengagement-3cq8.onrender.com/assets/remoteEntry.js'
         // businessAndEvents: 'http://localhost:3003/assets/remoteEntry.js'
       },
       shared: ['react', 'react-dom', '@apollo/client', 'graphql'],
